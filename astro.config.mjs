a7b12b8833ea0299192296651e5bd2844610de66
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-10-28 13:53:25 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@src': '/src',
+      }
+    }
+  },
+  server: {
+    host: true, // Écoute sur toutes les interfaces (0.0.0.0)
+    port: 8088   // Port utilisé par Apache ProxyPass
+  },
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
