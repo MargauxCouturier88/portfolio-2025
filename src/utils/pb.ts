38eb34834612058926ea72138cf34b420aceb555
MargauxCouturier88 <margaux.couturier02@edu.univ-fcomte.fr>
2025-11-07 09:31:06 +0100

    test

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,6 +3,6 @@ type TypedPocketBase = InstanceType<typeof PocketBase>;
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'http://localhost:8086'   //localhost = machine de déploiement
+else path = 'https://portfolio.margaux-couturier.fr:443'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
